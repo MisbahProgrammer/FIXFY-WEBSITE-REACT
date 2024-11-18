12e37b2eb27538a56dfcc0b80c431c4832ca616a
MisbahProgrammer <misbahrehman891@gmail.com>
2024-11-18 16:32:00 +0500

    Added a get quotation page

--- src/components/Button/Buttons.jsx
+++ src/components/Button/Buttons.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Getquote from '../GetQuote/Getquote'
 
 function buttons() {
   return (
     <>
     <button className='py-3 px-5 border-2 mx-3 rounded-xl hover:bg-cyan-600 hover:text-white font-bold font-sans'>Customer HUB</button>
-    <button className='py-3 px-5 border-2 font-sans bg-cyan-600 text-white rounded-xl hover:bg-slate-200 hover:text-gray-600 font-bold'>Get Quotation</button>
+    <button className='py-3 px-5 border-2 font-sans bg-cyan-600 text-white rounded-xl hover:bg-slate-200 hover:text-gray-600 font-bold'><a href="@">Get Quotation</a></button>
     </>
   )
 }
--- src/components/Copyright/Copyright.jsx
+++ src/components/Copyright/Copyright.jsx
@@ -4,8 +4,8 @@ function Copyright() {
   return (
     <div className='flex justify-around p-2'>
       <img src="fixfy-logo.png" alt="" width="60px" className='cursor-pointer' />
-        <p className='flex items-center justify-center text-xl'>&copy; Copyright 2022 Fixfy(pvt) Ltd.</p>
-        <p className='flex items-center justify-center text-xl'>Developed by<span className='text-cyan-800 p-3 '><a href="#" className='hover:text-cyan-500'>Misbah Ur Rehman</a></span></p>
+        <p className='flex items-center justify-center md:text-xl'>&copy; Copyright 2022 Fixfy(pvt) Ltd.</p>
+        <p className='flex items-center justify-center md:text-xl'>Developed by<span className='text-cyan-800 p-3 '><a href="#" className='hover:text-cyan-500'>Misbah Ur Rehman</a></span></p>
         
     </div>
   )
